2a37cf492c9d7d662334ea3819dd7bb6adb48e73
luodan@elinkport.com <linximeng>
2020-01-08 17:47:50 +0800

    权限导入

--- src/components/SetUp/PermissionsMange.vue
+++ src/components/SetUp/PermissionsMange.vue
@@ -1,84 +1,96 @@
 <template>
-    <div>
-        <div class="head">
-            <h1 class="head-title">{{$route.meta.title}}</h1>
-            <div class="head-action">
-                <el-input v-model="customFilters[0].vals" prefix-icon="el-icon-search" placeholder="搜索权限路由"
-                          class="class_input_width">
-                </el-input>
-                <el-button type="primary" @click="goSeed">新建权限</el-button>
-            </div>
-        </div>
-
-        <div class="content">
-            <data-tables-server :data="PermissionsData.ListData" :filters="customFilters" :total="PermissionsData.total"
-                                @query-change="getData"
-                                ref="multipleTable" v-loading="loading" :pagination-props="{ pageSizes: [5, 10, 20] }"
-                                :page-size="10">
-                <el-table-column prop="DisplayName" label="权限名称" key="DisplayName" sortable="custom">
-                    <template slot-scope="scope">
-                        <div class="blue cursor" @click="details(scope)"><b>{{scope.row.DisplayName}}</b></div>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="Name" label="路由" key="Name" sortable="custom">
-                </el-table-column>
-                <el-table-column prop="CreatedAt" label="创建时间" key="CreatedAt" sortable="custom">
-                </el-table-column>
-                <el-table-column label="操作">
-                    <template slot-scope="scope">
-                        <div class="operation_box">
-                            <span @click="edit(scope.row)">编辑</span>
-                            <span @click="deletes(scope.row)">删除</span>
-                        </div>
-                    </template>
-                </el-table-column>
-            </data-tables-server>
-        </div>
-        <el-dialog title="权限详情" :visible.sync="previewcol" class="previewcol">
-            <div class="cl-box" style="width:100%;">
-                <div class="cl-row">
-                    <div class="cl-td">
-                        <p>权限名称</p>
-                    </div>
-                    <div class="cl-td">
-                        <p>{{colshowlog.DisplayName}}</p>
-                    </div>
-                </div>
-                <div class="cl-row">
-                    <div class="cl-td">
-                        <p>路由</p>
-                    </div>
-                    <div class="cl-td">
-                        <p>{{colshowlog.Name}}</p>
-                    </div>
-                </div>
-                <div class="cl-row">
-                    <div class="cl-td">
-                        <p>说明</p>
-                    </div>
-                    <div class="cl-td">
-                        <p>{{colshowlog.Description}}</p>
-                    </div>
-                </div>
-                <div class="cl-row">
-                    <div class="cl-td">
-                        <p>创建时间</p>
-                    </div>
-                    <div class="cl-td">
-                        <p>{{colshowlog.CreatedAt}}</p>
-                    </div>
-                </div>
-            </div>
-            <div slot="footer" class="dialog-footer">
-                <el-button @click="previewcol = false">取 消</el-button>
-                <el-button type="primary" @click="edit(colshowlog)">编 辑</el-button>
-            </div>
-        </el-dialog>
+  <div>
+    <div class="head">
+      <h1 class="head-title">{{$route.meta.title}}</h1>
+      <div class="head-action">
+        <el-input v-model="customFilters[0].vals" prefix-icon="el-icon-search" placeholder="搜索权限路由"
+                  class="class_input_width">
+        </el-input>
+        <el-button type="primary" @click="goSeed">新建权限</el-button>
+      </div>
+      <el-upload
+        action="http://localhost:8081/v1/admin/permissions/import"
+        :on-preview="handlePreview"
+        :on-remove="handleRemove"
+        :before-remove="beforeRemove"
+        multiple
+        :limit="1"
+        :on-exceed="handleExceed"
+        :file-list="fileList">
+        <el-button size="small" type="primary">点击上传</el-button>
+        <!--            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
+      </el-upload>
     </div>
+
+    <div class="content">
+      <data-tables-server :data="PermissionsData.ListData" :filters="customFilters" :total="PermissionsData.total"
+                          @query-change="getData"
+                          ref="multipleTable" v-loading="loading" :pagination-props="{ pageSizes: [5, 10, 20] }"
+                          :page-size="10">
+        <el-table-column prop="DisplayName" label="权限名称" key="DisplayName" sortable="custom">
+          <template slot-scope="scope">
+            <div class="blue cursor" @click="details(scope)"><b>{{scope.row.DisplayName}}</b></div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="Name" label="路由" key="Name" sortable="custom">
+        </el-table-column>
+        <el-table-column prop="CreatedAt" label="创建时间" key="CreatedAt" sortable="custom">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <div class="operation_box">
+              <span @click="edit(scope.row)">编辑</span>
+              <span @click="deletes(scope.row)">删除</span>
+            </div>
+          </template>
+        </el-table-column>
+      </data-tables-server>
+    </div>
+    <el-dialog title="权限详情" :visible.sync="previewcol" class="previewcol">
+      <div class="cl-box" style="width:100%;">
+        <div class="cl-row">
+          <div class="cl-td">
+            <p>权限名称</p>
+          </div>
+          <div class="cl-td">
+            <p>{{colshowlog.DisplayName}}</p>
+          </div>
+        </div>
+        <div class="cl-row">
+          <div class="cl-td">
+            <p>路由</p>
+          </div>
+          <div class="cl-td">
+            <p>{{colshowlog.Name}}</p>
+          </div>
+        </div>
+        <div class="cl-row">
+          <div class="cl-td">
+            <p>说明</p>
+          </div>
+          <div class="cl-td">
+            <p>{{colshowlog.Description}}</p>
+          </div>
+        </div>
+        <div class="cl-row">
+          <div class="cl-td">
+            <p>创建时间</p>
+          </div>
+          <div class="cl-td">
+            <p>{{colshowlog.CreatedAt}}</p>
+          </div>
+        </div>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="previewcol = false">取 消</el-button>
+        <el-button type="primary" @click="edit(colshowlog)">编 辑</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-    import {mapState, mapActions} from 'vuex'
+    import {mapActions, mapState} from 'vuex'
     import api from '@/utils/api'
 
     export default {
@@ -107,6 +119,16 @@
                 previewcol: false,
                 //存放弹出框的数据
                 colshowlog: {},
+                // fileList: [
+                //     {
+                //         name: 'food.jpeg',
+                //         url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+                //     },
+                //     {
+                //         name: 'food2.jpeg',
+                //         url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+                //     }
+                // ],
             }
         },
         computed: {
@@ -171,7 +193,19 @@
                         id: row.ID
                     }
                 })
-            }
+            },
+            // handleRemove(file, fileList) {
+            //     console.log(file, fileList);
+            // },
+            // handlePreview(file) {
+            //     console.log(file);
+            // },
+            // handleExceed(files, fileList) {
+            //     this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            // },
+            // beforeRemove(file, fileList) {
+            //     return this.$confirm(`确定移除 ${file.name}？`);
+            // }
         },
         mounted() {
             this.getData()
@@ -181,87 +215,101 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-    .AddGroup {
-        color: rgb(60, 152, 255);
-        font-size: 14px;
-        margin-top: 10px;
-        cursor: pointer;
-        &-div {
-            margin-top: 10px;
-            &-input {
-                width: 40%;
-            }
-            &-add {
-                color: rgb(60, 152, 255);
-                font-size: 14px;
-                margin-top: 10px;
-                cursor: pointer;
-                margin-left: 15px;
-            }
-        }
-    }
+  .AddGroup {
+    color: rgb(60, 152, 255);
+    font-size: 14px;
+    margin-top: 10px;
+    cursor: pointer;
 
-    .class_input_width {
-        width: 350px;
-    }
+  &
+  -div {
+    margin-top: 10px;
 
-    .operation_box span {
-        cursor: pointer;
-        color: #8E9EBB;
-        margin-right: 10px;
-    }
+  &
+  -input {
+    width: 40%;
+  }
 
-    .head {
-        height: 70px;
-        background-color: #fff;
-        line-height: 70px;
-        padding: 0 30px;
-        margin-top: 10px;
-        display: flex;
+  &
+  -add {
+    color: rgb(60, 152, 255);
+    font-size: 14px;
+    margin-top: 10px;
+    cursor: pointer;
+    margin-left: 15px;
+  }
 
-        & h1 {
-            font-size: 20px;
-            color: rgb(16, 16, 16);
-            font-weight: 400;
-            width: 180px;
-        }
+  }
+  }
 
-        &-action {
-            flex: 1;
-            text-align: right;
+  .class_input_width {
+    width: 350px;
+  }
 
-            &-search {
-                width: 240px;
-                margin-right: 10px;
-            }
-        }
-    }
+  .operation_box span {
+    cursor: pointer;
+    color: #8E9EBB;
+    margin-right: 10px;
+  }
 
-    .content {
-        background-color: #fff;
-        overflow: hidden;
-        margin: 10px;
-        padding: 15px;
-        border-radius: 4px;
+  .head {
+    height: 70px;
+    background-color: #fff;
+    line-height: 70px;
+    padding: 0 30px;
+    margin-top: 10px;
+    display: flex;
 
-        &-select {
-            padding: 10px 15px;
-            background-color: rgb(246, 247, 248);
-            position: relative;
+  &
+  h1 {
+    font-size: 20px;
+    color: rgb(16, 16, 16);
+    font-weight: 400;
+    width: 180px;
+  }
 
-            .line {
-                position: absolute;
-                width: 1px;
-                height: 24px;
-                background: rgb(187, 187, 187);
-                left: 110px;
-                top: 18px;
-            }
+  &
+  -action {
+    flex: 1;
+    text-align: right;
 
-            &-button {
-                display: inline-block;
-                margin-left: 60px;
-            }
-        }
-    }
+  &
+  -search {
+    width: 240px;
+    margin-right: 10px;
+  }
+
+  }
+  }
+
+  .content {
+    background-color: #fff;
+    overflow: hidden;
+    margin: 10px;
+    padding: 15px;
+    border-radius: 4px;
+
+  &
+  -select {
+    padding: 10px 15px;
+    background-color: rgb(246, 247, 248);
+    position: relative;
+
+  .line {
+    position: absolute;
+    width: 1px;
+    height: 24px;
+    background: rgb(187, 187, 187);
+    left: 110px;
+    top: 18px;
+  }
+
+  &
+  -button {
+    display: inline-block;
+    margin-left: 60px;
+  }
+
+  }
+  }
 </style>
